5f90aba714ebb09dbb8a0e4779aedc49a09e3add
David Marshall <davidandkatharinemarshall@gmail.com>
2022-07-18 08:13:31 +0100

    Tidy up default app setup

--- modal-project/modal-project/src/App.vue
+++ modal-project/modal-project/src/App.vue
@@ -1,16 +1,21 @@
 <template>
+<!--
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+-->
+  <h1>My first Vue App</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- import HelloWorld from './components/HelloWorld.vue' -->
 
 export default {
   name: 'App',
+/*  
   components: {
     HelloWorld
   }
+*/
 }
 </script>
 

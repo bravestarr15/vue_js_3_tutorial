6bb00a71da9b8a7faad60caf5efe35f0ec1d72fc
David Marshall <davidandkatharinemarshall@gmail.com>
2022-07-20 17:29:56 +0100

    Instead of scoped, be more specific

--- modal-project/modal-project/src/components/Modal.vue
+++ modal-project/modal-project/src/components/Modal.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<style scoped>
+<style>
   .modal {
     width: 400px;
     padding: 20px;
@@ -20,5 +20,10 @@
     background: rgba(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
   }
 </style>

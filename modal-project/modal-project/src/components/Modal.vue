b6452163a7aaff399d75e2d582407328d69d12a7
David Marshall <davidandkatharinemarshall@gmail.com>
2022-07-20 19:43:46 +0100

    Additional styling

--- modal-project/modal-project/src/components/Modal.vue
+++ modal-project/modal-project/src/components/Modal.vue
@@ -40,11 +40,32 @@ export default {
     border: none;
     padding: 0;
   }
+  .modal p {
+    font-style: normal;
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
   .modal.sale {
     background: crimson;
     color: white;
   }
   .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
     color: white;
   }
 </style>

ca2d4f46f39c0729f906e5ad65faa78c3db1e2ff
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-09 10:07:18 +0100

    Initial Commit - vue router

---
+++ ninja-jobs/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa beatae voluptatum iste quibusdam, nisi animi pariatur id amet impedit, temporibus unde nulla perspiciatis ad dolores vero quo, perferendis illo dignissimos? Et expedita facilis saepe amet repellat sunt fugiat consequatur dicta adipisci architecto porro eaque molestias, delectus, velit unde placeat eveniet cumque sequi. Repellat doloremque pariatur placeat ipsum natus, odit non corporis recusandae, quam perferendis autem quod iste vero modi maiores? Qui sequi officia voluptas alias minus corporis eveniet explicabo, itaque, commodi deleniti cumque nemo eius, a repudiandae libero hic dicta magnam. Sunt voluptatibus laudantium possimus et, nulla fugit ex quis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa beatae voluptatum iste quibusdam, nisi animi pariatur id amet impedit, temporibus unde nulla perspiciatis ad dolores vero quo, perferendis illo dignissimos? Et expedita facilis saepe amet repellat sunt fugiat consequatur dicta adipisci architecto porro eaque molestias, delectus, velit unde placeat eveniet cumque sequi. Repellat doloremque pariatur placeat ipsum natus, odit non corporis recusandae, quam perferendis autem quod iste vero modi maiores? Qui sequi officia voluptas alias minus corporis eveniet explicabo, itaque, commodi deleniti cumque nemo eius, a repudiandae libero hic dicta magnam. Sunt voluptatibus laudantium possimus et, nulla fugit ex quis.</p>
+  </div>
+</template>
---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa beatae voluptatum iste quibusdam, nisi animi pariatur id amet impedit, temporibus unde nulla perspiciatis ad dolores vero quo, perferendis illo dignissimos? Et expedita facilis saepe amet repellat sunt fugiat consequatur dicta adipisci architecto porro eaque molestias, delectus, velit unde placeat eveniet cumque sequi. Repellat doloremque pariatur placeat ipsum natus, odit non corporis recusandae, quam perferendis autem quod iste vero modi maiores? Qui sequi officia voluptas alias minus corporis eveniet explicabo, itaque, commodi deleniti cumque nemo eius, a repudiandae libero hic dicta magnam. Sunt voluptatibus laudantium possimus et, nulla fugit ex quis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa beatae voluptatum iste quibusdam, nisi animi pariatur id amet impedit, temporibus unde nulla perspiciatis ad dolores vero quo, perferendis illo dignissimos? Et expedita facilis saepe amet repellat sunt fugiat consequatur dicta adipisci architecto porro eaque molestias, delectus, velit unde placeat eveniet cumque sequi. Repellat doloremque pariatur placeat ipsum natus, odit non corporis recusandae, quam perferendis autem quod iste vero modi maiores? Qui sequi officia voluptas alias minus corporis eveniet explicabo, itaque, commodi deleniti cumque nemo eius, a repudiandae libero hic dicta magnam. Sunt voluptatibus laudantium possimus et, nulla fugit ex quis.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>

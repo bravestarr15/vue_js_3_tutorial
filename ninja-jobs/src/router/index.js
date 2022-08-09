c7b786ff2a0381563b1421d6530e025111bc1491
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-09 15:49:54 +0100

    Introduce folder structure

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import AboutView from '../views/About.vue'
-import JobsView from '../views/Jobs.vue'
+import JobsView from '../views/jobs/Jobs.vue'
 
 const routes = [
   {

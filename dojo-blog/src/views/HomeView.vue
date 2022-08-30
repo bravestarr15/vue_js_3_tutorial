0fdda6c310fcee69fd583ff69946c8bc805dcebf
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-30 07:29:46 +0100

    Setup() fires first

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -7,5 +7,16 @@
 <script>
 export default {
   name: 'HomeView',
+  /* setup fires first, before anything else */
+  /* check console !! */
+  setup() {
+    console.log("setup")
+  },  
+  created() {
+    console.log("created")
+  },
+  mounted() {
+    console.log("mounted")
+  }
 }
 </script>

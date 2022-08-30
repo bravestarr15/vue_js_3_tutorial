93625eb266cc988a281c6c4e9967567092749076
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-30 08:25:55 +0100

    Update paragraph with reactive references

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -17,11 +17,17 @@ export default {
     /* you can't reference p here, since it hasn't been returned to the dom yet */
 
     /* note: these variables are not yet reactive; if their values update the paragraph in the template will not update  */
+/*
     let name = 'mario'
     let age = 30
+*/
+    /* make them reactive by using refs */
+    const name = ref('mario')
+    const age = ref(30)
 
     const handleClick = () => {
-
+      name.value = 'luigi'
+      age.value = 35
     }
 
     return { name, age, handleClick, p }

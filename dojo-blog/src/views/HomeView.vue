4c3a1bfd03960501ee2bab5828780c0f84035ad7
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-30 07:53:36 +0100

    import ref and create constant to hold a null ref

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'HomeView',
   /* setup fires first, before anything else */
@@ -14,6 +16,8 @@ export default {
 
     /* this is undefined in setup */
     console.log(this)
+
+    const p = ref(null)
 
     /* note: these variables are not yet reactive; if their values update the paragraph in the template will not update  */
     let name = 'mario'

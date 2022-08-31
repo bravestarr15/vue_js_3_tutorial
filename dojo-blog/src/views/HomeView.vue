27f0039d961c5095108f0becf30c79315a518bad
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-31 07:53:37 +0100

    Housekeeping setup for props example

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,41 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term: {{ search }}</p>
-    <div v-for="name in matchingNames" v-bind:key="name">{{ name }}</div>
-    <button @click="handleClick">stop watching</button>
+
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed, watch, watchEffect } from 'vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  /* setup fires first, before anything else */
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    const stopWatchEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopWatchEffect()
-    }
-
-    return { names, search, matchingNames, stopWatch, stopWatchEffect, handleClick }
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
+    ])
+ 
+    return { posts }
   }
 }
 </script>

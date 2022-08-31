5b43fac22f66d84988a1db60f62f33b81893d6de
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-31 07:31:22 +0100

    Simple computed values example

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
 export default {
   name: 'HomeView',
   /* setup fires first, before anything else */
   setup() {
+    const name = computed(() => {
+      return 'shaun'
+    })
 
+    return { name }
   }
 }
 </script>

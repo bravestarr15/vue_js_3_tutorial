3bc8d8f2573616a68dab93f0a109d6035fdc4602
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-31 07:36:39 +0100

    Setup search input box

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <input type="text" v-model="search">
     <div v-for="name in names" v-bind:key="name">{{ name }}</div>
   </div>
 </template>
@@ -12,9 +13,10 @@ export default {
   name: 'HomeView',
   /* setup fires first, before anything else */
   setup() {
+    const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    return { names }
+    return { names, search }
   }
 }
 </script>

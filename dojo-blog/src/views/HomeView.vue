e29848bd745c85ce66dcca93b3f8f2c4fa14ec81
David Marshall <davidandkatharinemarshall@gmail.com>
2022-08-31 07:34:52 +0100

    Set up more complex computed example

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{ name }}</p>
+    <div v-for="name in names" v-bind:key="name">{{ name }}</div>
   </div>
 </template>
 
@@ -12,11 +12,9 @@ export default {
   name: 'HomeView',
   /* setup fires first, before anything else */
   setup() {
-    const name = computed(() => {
-      return 'shaun'
-    })
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    return { name }
+    return { names }
   }
 }
 </script>

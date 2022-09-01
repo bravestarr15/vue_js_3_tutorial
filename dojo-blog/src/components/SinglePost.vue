d7a2da0f44176179130ae789dec2b483117fe0e7
David Marshall <davidandkatharinemarshall@gmail.com>
2022-09-01 08:34:48 +0100

    Use setup func on props to create snippet of post

--- dojo-blog/src/components/SinglePost.vue
+++ dojo-blog/src/components/SinglePost.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="post">
     <h3>{{ post.title }}</h3>
+    <p>{{ snippet }}</p>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
-  props: ['post']
+  props: ['post'],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + '...' /* 100 characters from position 0 in the string */
+    })
+
+    return { snippet }
+  }
 }
 </script>
 

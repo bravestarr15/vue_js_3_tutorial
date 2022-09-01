7d38437e1fb5da045f0fb57c58bb1319d503affa
David Marshall <davidandkatharinemarshall@gmail.com>
2022-09-01 08:42:27 +0100

    Other lifecycle hooks

--- dojo-blog/src/components/PostList.vue
+++ dojo-blog/src/components/PostList.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted, onUpdated } from 'vue'
 import SinglePost from './SinglePost.vue'
 
 export default {
@@ -16,6 +16,8 @@ export default {
   components: { SinglePost },
   setup(props) {
     onMounted(() => console.log('component mounted in setup function'))
+    onUnmounted(() => console.log('component unmounted in setup function'))
+    onUpdated(() => console.log('component updated in setup function'))
   },
   mounted() {
     console.log('component mounted outside setup function')

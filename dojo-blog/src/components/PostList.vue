6a3057633c663e2970c128c9911bc3bf96672507
David Marshall <davidandkatharinemarshall@gmail.com>
2022-09-01 08:25:50 +0100

    Pass post as props to new component

--- dojo-blog/src/components/PostList.vue
+++ dojo-blog/src/components/PostList.vue
@@ -2,18 +2,24 @@
   <div class="post-list">
     <div v-for="post in posts" v-bind:key="post.id">
       <h3>{{ post.title }}</h3>
+      <SinglePost v-bind:post="post" />
     </div>
   </div>
 </template>
 
 <script>
+
+import SinglePost from './SinglePost.vue'
+
 export default {
   props: ['posts'],
+  components: { SinglePost },
   setup(props) {
     console.log(props)
     console.log(props.posts)
   }
 }
+
 </script>
 
 <style>
---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['post']
+}
+</script>
+
+<style>
+
+</style>

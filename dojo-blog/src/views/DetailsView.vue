1737e448e5894152e875cc7f88687c4ca61b1fd9
David Marshall <davidandkatharinemarshall@gmail.com>
2022-09-04 19:01:21 +0100

    Import and call single post reusable

--- dojo-blog/src/views/DetailsView.vue
+++ dojo-blog/src/views/DetailsView.vue
@@ -5,10 +5,16 @@
 </template>
 
 <script>
+import getPost from '../composables/getPost'
+
 export default {
   props: ['id'], 
   setup(props) {
+    const { post, error, load } = getPost(props.id)
 
+    load()
+
+    return { post, error }
   }
 }
 </script>

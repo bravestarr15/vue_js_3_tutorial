467e27dc098a5811812a668c6a64c4fd64650c4e
David Marshall <davidandkatharinemarshall@gmail.com>
2022-09-04 19:04:33 +0100

    Present the returned blog onto the component

--- dojo-blog/src/views/DetailsView.vue
+++ dojo-blog/src/views/DetailsView.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    details
+    <div v-if="error">{{ error }}</div>
+    <div v-if="post" class="post">
+      <h3>{{ post.title }}</h3>
+      <p class="pre">{{ post.body}}</p>
+    </div>
   </div>
 </template>
 
